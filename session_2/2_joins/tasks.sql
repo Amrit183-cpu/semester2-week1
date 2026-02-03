1a4badc598ce727b398f8b4fa61ca6be2e31eb25
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-02-03 20:02:25 +0000

    mid joins

--- session_2/2_joins/tasks.sql
+++ session_2/2_joins/tasks.sql
@@ -7,3 +7,7 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+--1 select id from posts where deleted=1;
+--2 select username from users where username like 'a%';
+--3 select posts.id, users.username from posts join users on posts.user_id = users.id where content like '%small%';
+--4 select posts.id, users.username from posts join users on posts.user_id = users.id where deleted=0 order by posts.created_on desc;
