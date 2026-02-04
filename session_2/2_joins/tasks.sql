6dbab4a764d116a61a667fc88372a9ee8550d2c5
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-02-04 14:23:49 +0000

    session 2

--- session_2/2_joins/tasks.sql
+++ session_2/2_joins/tasks.sql
@@ -11,3 +11,18 @@
 --2 select username from users where username like 'a%';
 --3 select posts.id, users.username from posts join users on posts.user_id = users.id where content like '%small%';
 --4 select posts.id, users.username from posts join users on posts.user_id = users.id where deleted=0 order by posts.created_on desc;
+--5 select users.username, count(posts.user_id) from posts join users on posts.user_id = users.id group by users.username;
+--6 select users.username, count(posts.user_id) as num_of_posts from posts join users on posts.user_id = users.id group by users.username having num_of_posts>3;
+--7 select users.username, count(follows.followed_user_id) as following from users left join follows on users.id = follows.following_user_id group by users.username;
+--8 select users.username, count(follows.followed_user_id) as followers from users join follows on users.id = follows.followed_user_id group by users.username order by followers desc limit 5;
+--9 
+--for each user, show their username 
+--and the username of the user following them
+
+select users.username, follows.followed_user_id
+from users 
+join follows on users.id = follows.followed_user_id
+group by users.username limit 1;
+
+select * from users join follows on users.id = followed_user_id where username='baldwinkelsey' or users.id=17
+
